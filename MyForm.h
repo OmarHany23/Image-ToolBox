e7a825b2523c6413f457e255bee544fde4741dba
Omar Hany <omarhany2k@gmail.com>
2023-08-05 20:44:29 +0300

    adding all of the main project libraries

---
+++ MyForm.h
@@ -0,0 +1,19 @@
+//omar hany mohamed hussien
+//image processing techniques
+//customized tool box
+
+
+#pragma once
+#include<opencv2/opencv.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<Windows.h>
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<array>
+#include<vector>
+#include<set>
+#include<tchar.h>
+#include<math.h>
